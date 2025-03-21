6312e7e20fef231aab2947eb137b762d16767f36
Lior Goldemberg <lior.goldemberg@global-e.com>
2025-03-21 14:22:04 +0200

    5.json

--- words/64cdd390-6bb7-4a8b-b0e0-b52294368613/2025-mar-en-5.js
+++ words/64cdd390-6bb7-4a8b-b0e0-b52294368613/2025-mar-en-5.js
@@ -104,4 +104,116 @@ words = [
             }
         ]
     }
+    ,
+    {
+        "text": "go",
+        "translation": "went",
+        "partOfSpeech": "verb",
+        "sentences": [
+            {
+                "from": "We went to the park yesterday.",
+                "to": "הלכנו לפארק אתמול."
+            }
+        ]
+    },
+    {
+        "text": "eat",
+        "translation": "ate",
+        "partOfSpeech": "verb",
+        "sentences": [
+            {
+                "from": "She ate an apple for breakfast.",
+                "to": "היא אכלה תפוח לארוחת בוקר."
+            }
+        ]
+    },
+    {
+        "text": "write",
+        "translation": "wrote",
+        "partOfSpeech": "verb",
+        "sentences": [
+            {
+                "from": "He wrote a letter to his friend.",
+                "to": "הוא כתב מכתב לחבר שלו."
+            }
+        ]
+    },
+    {
+        "text": "drive",
+        "translation": "drove",
+        "partOfSpeech": "verb",
+        "sentences": [
+            {
+                "from": "She drove to work yesterday.",
+                "to": "היא נהגה לעבודה אתמול."
+            }
+        ]
+    },
+    {
+        "text": "become",
+        "translation": "became",
+        "partOfSpeech": "verb",
+        "sentences": [
+            {
+                "from": "He became a doctor after many years of study.",
+                "to": "הוא הפך לרופא אחרי שנים רבות של לימודים."
+            }
+        ]
+    },
+    {
+        "text": "fly",
+        "translation": "flew",
+        "partOfSpeech": "verb",
+        "sentences": [
+            {
+                "from": "They flew to London last year.",
+                "to": "הם טסו ללונדון בשנה שעברה."
+            }
+        ]
+    },
+    {
+        "text": "take",
+        "translation": "took",
+        "partOfSpeech": "verb",
+        "sentences": [
+            {
+                "from": "He took a pen from the table.",
+                "to": "הוא לקח עט מהשולחן."
+            }
+        ]
+    },
+    {
+        "text": "dream",
+        "translation": "dreamt",
+        "partOfSpeech": "verb",
+        "sentences": [
+            {
+                "from": "He dreamt about a giant cake.",
+                "to": "הוא חלם על עוגה ענקית."
+            }
+        ]
+    },
+    {
+        "text": "lose",
+        "translation": "lost",
+        "partOfSpeech": "verb",
+        "sentences": [
+            {
+                "from": "I lost my keys yesterday.",
+                "to": "איבדתי את המפתחות שלי אתמול."
+            }
+        ]
+    },
+    {
+        "text": "sit",
+        "translation": "sat",
+        "partOfSpeech": "verb",
+        "sentences": [
+            {
+                "from": "She sat on the chair.",
+                "to": "היא ישבה על הכיסא."
+            }
+        ]
+    }
 ]
+    
